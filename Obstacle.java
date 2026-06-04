7e56042be4f61bfdd4560e8d50e62deab321be6a
Arnav Agarwal <talkwitharnav@gmail.com>
2026-06-04 15:02:15 -0700

    Adding another obstacle constructor for custom obstacles

--- Obstacle.java
+++ Obstacle.java
@@ -3,8 +3,18 @@ public class Obstacle {
     private boolean isObstacle;
     private ArrayList<String> obstacleTypes = new ArrayList<String>(); // List of possible obstacle types
 
+    // Default constructor that initializes the list with some default obstacles
     public Obstacle() {
         obstacleTypes.add("Snake");
-        obstacleTypes.add()
+        obstacleTypes.add("Spike");
+        obstacleTypes.add("Banana Peel");
+    }
+
+    // This constructor takes in string input of all obstacles seperated by , eg "Banana, Peel, Spike, Kid", then throws it into arraylist
+    public Obstacle(String obstacles) {
+        String[] obstacleArray = obstacles.split(","); // Split the input string into an array
+        for (String obstacle : obstacleArray) {
+            obstacleTypes.add(obstacle.trim()); // Add each obstacle to the list, trimming whitespace
+        }
     }
 }
