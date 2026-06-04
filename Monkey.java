1ea15aa5fb89c56a0cd682c7a39f88ab65458680
talkwitharnav-web <talkwitharnav@gmail.com>
2026-06-03 21:41:45 -0700

    making constructors for monkey and obstacle

--- Monkey.java
+++ Monkey.java
@@ -3,4 +3,12 @@ public class Monkey {
     private int lives = 3; // The monkey starts with 3 lives.
     private int score = 0; // The score of the monkey
     private int currentBar = 0; // Which bar the monkey currently is on.
+
+    public Monkey() {
+        // Default constructor
+    }
+    
+    public Monkey(int lives) {
+        this.lives = lives;
+    }
 }
--- Obstacle.java
+++ Obstacle.java
@@ -3,4 +3,8 @@ public class Obstacle {
     private boolean isObstacle;
     private ArrayList<String> obstacleTypes = new ArrayList<String>(); // List of possible obstacle types
 
+    public Obstacle() {
+        obstacleTypes.add("Snake");
+        obstacleTypes.add()
+    }
 }
