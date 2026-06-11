449a9dbe235cb3f37271ef00cac672fd5bedcf06
Arnav Agarwal <talkwitharnav@gmail.com>
2026-06-10 17:36:56 -0700

    repurposing the game to a story mode type of game.

--- Main.java
+++ Main.java
@@ -1,18 +1,1 @@
-import javax.swing.JFrame;
 
-// This is main class for the game.
-public class Main {
-    public static void main(String[] args) {
-        Monkey monkey = new Monkey();
-        Environment env = new Environment();
-
-        JFrame frame = new JFrame("Crossy Monkey");
-        GamePanel panel = new GamePanel(monkey, env);
-
-        frame.add(panel);
-        frame.setSize(800, 600);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
-    }
-}
